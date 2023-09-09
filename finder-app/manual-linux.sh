a2b31fb34438e6fff2b02580d09a3df4941b6bc1
Suchith.J.N <suchithjn22@gmail.com>
2023-09-09 10:18:24 +0530

    manual linux works

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -4,6 +4,7 @@
 
 set -e
 set -u
+set -x
 
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
@@ -34,7 +35,33 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    ### Build Linux kernel
+
+    # Deep clean the linux kernel. This removes all
+    # the build artifacts that were there before.
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    # Create default config for the ARM64 target
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    # Compile the kernel with the cross-compile toolchain
+    set +e
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    if [[ $? -ne 0 ]]; then
+        # Apply dtc-multiple-definition.patch
+        git apply ${FINDER_APP_DIR}/dtc-multiple-definition.patch
+        set -e; make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    fi
+    set -e
+
+    # Compile kernel modules which can be loaded at runtime
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+
+    # Compile device tree files to be loaded while running QEMU.
+    # Device tree file describes the hardware the Linux kernel is
+    # running on. It is required to configure hardware drivers. This
+    # is provided to the QEMU guest as DTB (Device Tree Blob)
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
@@ -46,35 +73,66 @@ then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
+mkdir -p ${OUTDIR}/rootfs
 
-# TODO: Create necessary base directories
+### Create necessary base directories as required by the
+### Filesystem Hierarchy Standard (FHS)
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+   
+    ### Build and configure busybox
+    # The last command builds the root filesystem
+    make distclean
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j$(nproc) ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+### Make and install busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+cd "${OUTDIR}/rootfs"
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+### Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
-# TODO: Make device nodes
+cp -Pvr ${SYSROOT}/lib/ ${OUTDIR}/rootfs
+cp -Pvr ${SYSROOT}/lib64/ ${OUTDIR}/rootfs
 
-# TODO: Clean and build the writer utility
+### Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+### Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+CROSS_COMPILE=${CROSS_COMPILE} make build
 
-# TODO: Chown the root directory
+### Copy the finder related scripts and executables to the /home directory
+### on the target rootfs
+cp conf/username.txt ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp writer ${OUTDIR}/rootfs/home
 
-# TODO: Create initramfs.cpio.gz
+### Chown the root directory
+sudo chown -R root:root ${OUTDIR}/rootfs
+
+### Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
+cd ${OUTDIR}
+gzip -f initramfs.cpio
