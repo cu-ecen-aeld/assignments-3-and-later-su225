829ab182572a779844e7897171eccbf346adc76a
Suchith.J.N <suchithjn22@gmail.com>
2023-09-15 20:14:30 +0530

    fix finder.sh path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -54,7 +54,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 cat ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
