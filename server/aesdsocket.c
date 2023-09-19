7e25432f7d8f5dc896763c535d7024dea52ac3ae
Suchith.J.N <suchithjn22@gmail.com>
2023-09-19 20:42:59 +0530

    fix socket program according to the spec

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -19,6 +19,70 @@
 #define MAX_IP_LENGTH         32
 #define MAX_DATABUFFER_SIZE   1024
 
+/** 
+ * Line buffer abstracts keeping the line in memory. This is
+ * important for handling long lines. It is a stripped down
+ * version of the string type in C++ - a vector of chars.
+ */
+struct line_buffer {
+  char*  line;
+  ssize_t line_len;
+  ssize_t line_cap;
+};
+
+void line_buffer_init(struct line_buffer *buff) {
+  buff->line = NULL;
+  buff->line_len = 0;
+  buff->line_cap = 0;
+}
+
+void line_buffer_clear(struct line_buffer *buff) {
+  buff->line_len = 0;
+  memset(buff->line, 0, buff->line_cap);
+}
+
+void line_buffer_destroy(struct line_buffer *buff) {
+  if (buff->line != NULL) {
+    free(buff->line);
+  }
+}
+
+int line_buffer_append(struct line_buffer *buff, char *c, size_t csz) {
+  if (csz == 0) {
+    return buff->line_len;
+  }
+  int new_size = buff->line_len + csz;
+  bool needs_resize = new_size > buff->line_cap;
+  if (needs_resize) {
+    int reqd_cap = buff->line_cap;
+    if (reqd_cap == 0) {
+      reqd_cap = 1;
+    }
+    while (reqd_cap < new_size) {
+      reqd_cap <<= 1;
+    }
+    char *newline = (char *)calloc(reqd_cap * sizeof(char));
+    if (newline == NULL) {
+      perror("line-buffer: malloc failed");
+      return -1;
+    }
+    memcpy(newline, buff->line, buff->line_len);
+    if (buff->line != NULL) {
+        free(buff->line);
+    }
+    buff->line = newline;
+    buff->line_cap = reqd_cap;
+  }
+  memcpy(buff->line + buff->line_len, c, csz);
+  buff->line_len = new_size;
+  return buff->line_len;
+}
+
+char *line_buffer_get(struct line_buffer *buff, ssize_t *len) {
+  *len = buff->line_len;
+  return buff->line;
+}
+
 void get_peer_address(struct sockaddr *addr, char *addr_buffer, int maxlen);
 
 int main(void) {
@@ -37,11 +101,6 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  // setup a nonblocking socket. This is because epoll and edge-triggered
-  // variant requires nonblocking so that either we read the data in the
-  // socket buffer in full or get EAGAIN instead of reading partially and
-  // getting blocked even when there is some data in the buffer. This
-  // example scenario is described in the epoll() manual.
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd == -1) {
     perror("cannot create server socket");
@@ -58,7 +117,7 @@ int main(void) {
     exit(EXIT_FAILURE);
   }
 
-  int outfilefd = open(AESD_DATAFILE_PATH, O_RDWR | O_CREAT, 0666);
+  int outfilefd = open(AESD_DATAFILE_PATH, O_CREAT | O_TRUNC | O_RDWR | O_APPEND, 0666);
   if (outfilefd == -1) {
     perror("error while opening the output file");
     exit(EXIT_FAILURE);
@@ -71,7 +130,13 @@ int main(void) {
   socklen_t       client_address_len = sizeof(client_address);
   char            client_addr_buffer[MAX_IP_LENGTH+1];
   char            data_buffer[MAX_DATABUFFER_SIZE];
+  
+  struct line_buffer lb;
+  ssize_t            line_len;
 
+  line_buffer_init(&lb);
+
+  ssize_t total_bytes_written = 0;
   while(1) {
     // We consider 1 client at a time model (very primitive) because it is
     // given that the data is written to /var/tmp/aesdsocketdata.
@@ -79,60 +144,59 @@ int main(void) {
     get_peer_address(&client_address, client_addr_buffer, MAX_IP_LENGTH+1);
     syslog(LOG_INFO, "Accepted connection from %s", client_addr_buffer);
 
-    // Now we read into the buffer and write to the file until we encounter
-    // a new line. Once we encounter a newline, we have to read the contents
-    // of the entire file and write it back to the client socket.
-    int bytes_read, bytes_written;
-    int total_bytes_read = 0;
-    bool write_error = false;
     while (1) {
-      bytes_read = read(clientsockfd, data_buffer, MAX_DATABUFFER_SIZE);
-      if (bytes_read <= 0) {
+      int bytes_read = read(clientsockfd, data_buffer, MAX_DATABUFFER_SIZE);
+      // There are no more bytes to read.
+      // We have reached the end-of-file.
+      if (bytes_read == 0) { 
         break;
       }
-      total_bytes_read += bytes_read;
+      // We have run into some trouble while
+      // reading from the socket.
+      if (bytes_read < 0) {
+        perror("error while reading from the socket");
+        break;
+      }
+      
+      // Read line-by-line and append each line to the file.
+      // On reading the newline we have to send the entire
+      // contents of the file to the socket.
+      int start = 0;
+      for (int i = 0; i < bytes_read; i++) {
+        if (data_buffer[i] != '\n') {
+          continue;
+        }
+        line_buffer_append(&lb, data_buffer+start, i-start);
+        char *line = line_buffer_get(&lb, &line_len);
+        int bytes_written = write(outfilefd, line, line_len);
+        if (bytes_written < 0) {
+          perror("error while appending line");
+          goto cleanup_client; 
+        }
+        if (bytes_written < line_len) {
+          syslog(LOG_WARNING, "writing entire bytes failed as the system is running out of disk space");
+        }
+        total_bytes_written += bytes_written;
+        line_buffer_clear(&lb);
 
-      // Write the data to the file
-      int bytes_to_write = bytes_read;
-      bool found_newline = false;
-      for (int i = 0; i < bytes_read; ++i) {
-        if (data_buffer[i] == '\n') {
-          bytes_to_write = i+1;
-          found_newline = true;
-          break;
+        // The spec says we have to immediately output the
+        // contents of the entire file to the socket
+        off_t fileoffset = 0;
+        while (fileoffset < total_bytes_written) {
+          int res = sendfile(clientsockfd, outfilefd, &fileoffset, total_bytes_written-fileoffset);
+          if (res < 0) {
+            perror("error while sending file output to socket");
+            goto cleanup_client;
+          }
         }
+
+        start = i+1;
       }
-      bytes_written = write(outfilefd, data_buffer, bytes_to_write);
-      if (bytes_written < 0) {
-        write_error = true;
-        perror("error while writing bytes to the client");
-        break;
-      }
-      if (found_newline) {
-        break;
-      }
+      line_buffer_append(&lb, data_buffer+start, bytes_read-start);
     }
-    if (bytes_read < 0) {
-      perror("error while reading from the client socket");
-    }
-    if (write_error) {
-      syslog(LOG_ERR, "Error while writing data from %s", client_addr_buffer);
-    } else {
-      // Now write the data back to clientsockfd
-      off_t offset = 0;
-      int bytes_to_pipe = total_bytes_read;
-      while (bytes_to_pipe > 0) {
-        ssize_t cur_piped = sendfile(clientsockfd, outfilefd, &offset, bytes_to_pipe);
-        if (cur_piped == -1) {
-          perror("error while echoing packet to the client");
-          syslog(LOG_ERR, "Error while echoing data from %s", client_addr_buffer);
-          break;
-        }
-        bytes_to_pipe -= cur_piped;
-      }
-    }
+  cleanup_client:
     close(clientsockfd);
-    truncate(AESD_DATAFILE_PATH, 0);
+    line_buffer_clear(&lb);
     syslog(LOG_INFO, "Closed connection from %s", client_addr_buffer);
   }
 
